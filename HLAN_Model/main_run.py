b915c88c55ba8fbf0efc90f8755fda47c44cd6d5
LeoJiny <leojinyiyang@outlook.com>
2024-10-30 19:21:23 -0500

    update main_run

--- HLAN_Model/main_run.py
+++ HLAN_Model/main_run.py
@@ -10,6 +10,7 @@ import gc
 import random
 import os
 from HLAN import HLAN
+import argparse
 
 def seed_everything(seed=42):
     import random, os
@@ -25,38 +26,7 @@ def seed_everything(seed=42):
     torch.backends.cudnn.benchmark = True
 
 
-word_model_path ='/u/l/j/ljin/private/cs769/Embeddings/word-emb_model_weights.pt' ## Path to word-emb model
-code_model_path ='/u/l/j/ljin/private/cs769/Embeddings/code-emb_model_weights.pt' ## Path to code-emb model
-x_path ='/u/l/j/ljin/private/cs769/Embeddings/train_word-emb.pt' ## Path to train_word-emb 
-y_path = '/u/l/j/ljin/private/cs769/Embeddings/train_code-emb.pt'## Path to train_code-emb 
-word_weight_tensor = torch.load(word_model_path, weights_only = True)
-code_weight_tensor = torch.load(code_model_path, weights_only=True)
-x = torch.load(x_path, weights_only = True)
-y = torch.load(y_path, weights_only = True).float()
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-# device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
-print("Using device:", device)
-
-### Hyperparameters:
-### Define hidden size and embedding dim
-hidden_size = 100
-embed_dim = 100
-batch_size = 12
-num_sent = 25
-lr = 0.01
-dropout_prob = 0.5
-l2 = 0.0001
-calibration = 0.5
-num_epochs = 100
-
-seed_everything()
-model = HLAN(num_sent = num_sent, word_weight_tensor=word_weight_tensor,code_weight_tensor=code_weight_tensor,embed_dim=embed_dim,hidden_size=hidden_size,dropout_prob=dropout_prob)
-model.to(device)
-
-loss_fn = nn.BCELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr = lr, weight_decay = l2)
-def train_model(model, train_dataloader, dev_dataloader,optimizer, loss_fn, num_epochs, device):
+def train_model(model, train_dataloader, dev_dataloader,optimizer, loss_fn, num_epochs, device,calibration):
     seed_everything()
     train_loss = list()
     dev_micro_f1, dev_macro_f1, dev_micro_auroc,dev_macro_auroc = list(),list(),list(),list()
@@ -143,14 +113,118 @@ def evaluate_model(model, dataloader, calibration, device):
     # print(all_predictions)
     return micro_f1,macro_f1,micro_auroc,macro_auroc
 
-best_micro_f1 = 0.0
-dataset = torch.utils.data.TensorDataset(x,y)
-dataloader = torch.utils.data.DataLoader(dataset,batch_size = 12, shuffle = True)
-eval_dataloader = torch.utils.data.DataLoader(dataset,batch_size = batch_size, shuffle = False)
-seed_everything()
-train_loss, dev_micro_f1,dev_macro_f1,dev_micro_auroc,dev_macro_auroc = train_model(model, dataloader, eval_dataloader,optimizer, loss_fn, num_epochs, device)
-np.save('train_loss_'+str(num_epochs)+'.npy',train_loss)
-np.save('dev_micro_f1_'+str(num_epochs)+'.npy',dev_micro_f1)
-np.save('dev_macro_f1_'+str(num_epochs)+'.npy',dev_macro_f1)
-np.save('dev_micro_auroc_'+str(num_epochs)+'.npy',dev_micro_auroc)
-np.save('dev_macro_auroc_'+str(num_epochs)+'.npy',dev_macro_auroc)
+### Hyperparameters:
+### Define hidden size and embedding dim
+def main(args):
+    hidden_size = args.hidden_size
+    embed_dim = args.embed_dim
+    batch_size = args.batch_size
+    num_sent = args.num_sent
+    lr = args.lr
+    dropout_prob = args.dropout
+    l2 = args.l2
+    calibration = args.calibration
+    num_epochs = args.num_epochs
+    use_label_embeddings= args.use_label
+    freeze_emb = args.freeze_emb
+    scenario = ''
+    if use_label_embeddings:
+        scenario += '_use_label_emb_'
+    if freeze_emb:
+        scenario += '_freeze_label_emb'
+
+    word_model_path ='word-emb_model_weights.pt' ## Path to word-emb model
+    code_model_path ='Embeddings/code-emb_model_weights.pt' ## Path to code-emb model
+    train_x_path ='train_word-emb.pt' ## Path to train_word-emb 
+    train_y_path = 'train_code-emb.pt'## Path to train_code-emb 
+    word_weight_tensor = torch.load(word_model_path, weights_only = True)
+    code_weight_tensor = torch.load(code_model_path, weights_only=True)
+    train_x = torch.load(train_x_path, weights_only = True)
+    train_y = torch.load(train_y_path, weights_only = True).float()
+    dev_x_path ='dev_word-emb.pt' ## Path to dev_word-emb 
+    dev_y_path = 'dev_code-emb.pt'## Path to dev_code-emb 
+    dev_x = torch.load(dev_x_path, weights_only = True)
+    dev_y = torch.load(dev_y_path, weights_only = True).float()
+    word_weight_tensor = torch.load(word_model_path, weights_only = True)
+    code_weight_tensor = torch.load(code_model_path, weights_only=True)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+    print("Using device:", device)
+    seed_everything()
+    model = HLAN(num_sent = num_sent, word_weight_tensor=word_weight_tensor,
+                 code_weight_tensor=code_weight_tensor,embed_dim=embed_dim,
+                 hidden_size=hidden_size,dropout_prob=dropout_prob,
+                 use_label_embeddings= use_label_embeddings,freeze_embed = freeze_emb)
+    model.to(device)
+    loss_fn = nn.BCELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr = lr, weight_decay = l2)
+    best_micro_f1 = 0.0
+    train_dataset = torch.utils.data.TensorDataset(train_x,train_y)
+    train_dataloader = torch.utils.data.DataLoader(train_dataset,batch_size = batch_size, shuffle = True)
+    dev_dataset = torch.utils.data.TensorDataset(dev_x,dev_y)
+    dev_dataloader = torch.utils.data.DataLoader(dev_dataset,batch_size = batch_size, shuffle = True)
+    seed_everything()
+    train_loss, dev_micro_f1,dev_macro_f1,dev_micro_auroc,dev_macro_auroc = train_model(model, train_dataloader, dev_dataloader,optimizer, loss_fn, num_epochs, device,calibration = calibration)
+    np.save('train_loss_'+str(num_epochs)+scenario+'.npy',train_loss)
+    np.save('dev_micro_f1_'+str(num_epochs)+scenario+'.npy',dev_micro_f1)
+    np.save('dev_macro_f1_'+str(num_epochs)+scenario+'.npy',dev_macro_f1)
+    np.save('dev_micro_auroc_'+str(num_epochs)+scenario+'.npy',dev_micro_auroc)
+    np.save('dev_macro_auroc_'+str(num_epochs)+scenario+'.npy',dev_macro_auroc)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--hidden_size',
+                        default = 100,
+                        type = int)
+    parser.add_argument('--embed_dim',
+                        default = 100,
+                        type = int)
+    parser.add_argument('--batch_size',
+                        default = 12,
+                        type = int)
+    parser.add_argument('--num_sent',
+                        default = 25,
+                        type = int)
+    parser.add_argument('--lr',
+                        default = 0.01,
+                        type = float)
+    parser.add_argument('--dropout',
+                        default = 0.5,
+                        type = float)
+    parser.add_argument('--l2',
+                        default = 0.0001,
+                        type = float)
+    parser.add_argument('--calibration',
+                        default = 0.5,
+                        type = float)
+    parser.add_argument('--num_epochs',
+                        default = 100,
+                        type = int)
+    parser.add_argument('--use_label',
+                        default = True,
+                        type = bool)
+    parser.add_argument('--freeze_emb',
+                        default = True,
+                        type = bool)
+    
+
+    main(parser.parse_args())
+
+    # hidden_size = 100
+    # embed_dim = 100
+    # batch_size = 12
+    # num_sent = 25
+    # lr = 0.01
+    # dropout_prob = 0.5
+    # l2 = 0.0001
+    # calibration = 0.5
+    # num_epochs = 100
+    # use_label_embeddings= args.use_label
+    # freeze_emb = args.freeze_emb
+
+
+
+
+
+
