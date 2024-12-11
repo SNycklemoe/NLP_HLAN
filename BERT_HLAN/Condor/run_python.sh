5a36dbca011b0250f4daa62bd484dbeef554ae80
SNycklemoe <158520673+SNycklemoe@users.noreply.github.com>
2024-12-11 16:21:35 -0600

    Delete BERT_HLAN/Condor directory

--- BERT_HLAN/Condor/run_python.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# run your script
-python3 main_run_bert.py --bert_emb $1 --use_label $2 --freeze_emb $3 --sentence_split $4 --dimension_reduction $5 --num_epochs $6 --lr 1e-3 --batch_size 4 --subset test
-# python3 main_run_bert1.py
